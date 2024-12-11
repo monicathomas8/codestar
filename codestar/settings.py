725149a36ae1e22043c8f92411d4ce0b68b65559
Monica Thomas <monicathomas8@icloud.com>
2024-12-11 14:41:09 +0000

    connected database

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-puci7-!7rs&a*u)-_w$@fkhppp!&4z&@mb2uini^usv$#=2-9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 
     '8000-monicathomas8-codestar-a52giwca62f.ws.codeinstitute-ide.net', 
